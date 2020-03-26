2797d4eca803f1368f901ed444690f4ba691ad03
Elmer Zapata <elmer.765@gmail.com>
2020-03-26 17:00:37 -0400

    fix for route

--- backend-xforce/routes/cuestionario.js
+++ backend-xforce/routes/cuestionario.js
@@ -7,7 +7,7 @@ var router = express.Router();
 router.post('/cuestionario', emergenciaController.saveCuestionario);
 router.get('/cuestionario/:id', emergenciaController.getCuestionario);
 router.put('/cuestionario/:id', emergenciaController.updateCuestionario);
-router.get('/cuestionario', emergenciaController.getCuestionarios);
+router.get('/cuestionarios', emergenciaController.getCuestionarios);
 router.delete('/cuestionario/:id', emergenciaController.deleteCuestionario);
 
 module.exports= router;
