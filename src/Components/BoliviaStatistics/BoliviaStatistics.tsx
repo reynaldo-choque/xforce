c2a07b19cfa187b23419e11a284624448c583897
Reynaldo Choque <reynaldo_choque@coxautoinc.com>
2020-04-08 20:49:33 -0400

    fix: data and styles

--- src/Components/BoliviaStatistics/BoliviaStatistics.tsx
+++ src/Components/BoliviaStatistics/BoliviaStatistics.tsx
@@ -1,4 +1,9 @@
 import * as React from 'react';
+
+import {
+    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
+
 import XforceAPI from "../../Services/XforceAPI";
 import MapChart from "../MapChart/MapChart";
 
@@ -7,6 +12,11 @@ import "./BoliviaStatistics.css";
 import boliviaStatisticsData from "../../data/boliviaStatisticsData.json";
 
 class BoliviaStatistics extends React.Component<any, any> {
+    state = {
+        chartSize: 200,
+        bottomChartSize: 600
+    };
+
     constructor(props: any) {
         super(props);
     }
@@ -15,10 +25,33 @@ class BoliviaStatistics extends React.Component<any, any> {
         // XforceAPI call rest for statistics
         //this.setState({
         //});
+
+        window.addEventListener('resize', this.handleWindowResize);
+        this.handleWindowResize();
+    }
+
+    handleWindowResize = () => {
+        console.log("resize", window.innerWidth);
+        let width = window.innerWidth;
+
+        if(width >= 1920)
+            width = 450;
+        else if (width >= 1600)
+            width = width * 25 / 100;
+        else if (width >= 1300)
+            width = width * 29 / 100;
+        else if (width >= 1000)
+            width = width * 35 / 100;
+        else
+            width = width * 36 / 100;
+        this.setState({
+            chartSize: width,
+            bottomChartSize:  window.innerWidth - 100
+        });
     }
 
     render() {
-        const {data: {generalInfo, byDepartment}} = boliviaStatisticsData;
+        const {data: {generalInfo, byDepartment, hystoricByDay}} = boliviaStatisticsData;
         return (
             <React.Fragment>
                 <div className="bolivia-statistics" >
@@ -82,8 +115,32 @@ class BoliviaStatistics extends React.Component<any, any> {
                                     <th>{generalInfo.muertes}</th>
                                 </tfoot>
                             </table>
+                            <div className="wrapper-sinple-chart">
+                                <LineChart width={this.state.chartSize} height={300} data={hystoricByDay}
+                                           margin={{top: 20, right: 5, left: -25, bottom: 5}}>
+                                    <XAxis dataKey="name"/>
+                                    <YAxis/>
+                                    <Tooltip />
+                                    <Legend />
+                                    <CartesianGrid strokeDasharray="1 1"/>
+                                    <Line type="monotone" dataKey="casos" stroke="orange" dot={{ stroke: 'orange', strokeWidth: 1 }} activeDot={{r: 1}}/>
+                                    <Line type="monotone" dataKey="muertes" stroke="purple" dot={{ stroke: 'purple', strokeWidth: 1 }} activeDot={{r: 1}}/>
+                                </LineChart>
+                            </div>
                         </div>
                     </div>
+                </div>
+                <div className="bottom-chart">
+                    <LineChart width={this.state.bottomChartSize} height={300} data={hystoricByDay}
+                               margin={{top: 20, right: 5, left: 5, bottom: 5}}>
+                        <XAxis dataKey="name"/>
+                        <YAxis/>
+                        <Tooltip />
+                        <Legend />
+                        <CartesianGrid strokeDasharray="1 1"/>
+                        <Line type="monotone" dataKey="casos" stroke="orange" dot={{ stroke: 'orange', strokeWidth: 1 }} activeDot={{r: 1}}/>
+                        <Line type="monotone" dataKey="muertes" stroke="purple" dot={{ stroke: 'purple', strokeWidth: 1 }} activeDot={{r: 1}}/>
+                    </LineChart>
                 </div>
                 <div className="bolivia-statistics by-department">
                     <table>
